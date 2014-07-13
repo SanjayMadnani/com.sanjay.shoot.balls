fe1eeb0f26b73a3e7c0fe5537c54aa7cdd2093ba
Sanjay Madnani <http://sanjaymadnani.wordpress.com/>
2014-07-14 02:32:57 +0530

    Sound file missing, Game is ready to be played,
    Signed-off-by: Sanjay Madnani <http://sanjaymadnani.wordpress.com/>

--- ShootTheBalls/src/appletShootBalls/Player.java
+++ ShootTheBalls/src/appletShootBalls/Player.java
@@ -1,40 +1,42 @@
-/*
- * Copyright (C) 2014, 2015 Sanjay Madnani
- *
- * This file is free to use: you can redistribute it and/or modify it under the terms of the 
+/* Copyright (C) 2014, 2015 Sanjay Madnani
+ * 
+ * This file is free to use: you can redistribute it and/or modify it under the terms of the
  * GPL General Public License V2 as published by the Free Software Foundation, subject to the following conditions:
- *                                                                                          
+ * 
  * The above copyright notice should never be changed and should always included wherever this file is used.
- *                                                                                          
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.  
- * See the GNU General Public License V2 for more details.                                       
- *
- */
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License V2 for more details. */
 package appletShootBalls;
 
+/**
+ * 
+ * @author SANJAY
+ * 
+ */
 public class Player {
-	private int score;
-	private int lives;
-	
-	public Player() {
-		lives = 10;
-		score = 0;
-	}
+    private int score;
+    private int lives;
 
-	public int getScore() {
-		return score;
-	}
+    public Player() {
+        lives = 10;
+        score = 0;
+    }
 
-	public void addScore(int score) {
-		this.score += score;
-	}
+    public int getScore() {
+        return score;
+    }
 
-	public int getLives() {
-		return lives;
-	}
+    public void addScore(int score) {
+        this.score += score;
+    }
 
-	public void looseLife() {
-		lives--;
-	}
-	
+    public int getLives() {
+        return lives;
+    }
+
+    public void looseLife() {
+        lives--;
+    }
+
 }
